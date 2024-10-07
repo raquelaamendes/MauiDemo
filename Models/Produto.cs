d9bbad1bb1c37372c0e6ab90f71268c359b77e4f
raquelaamendes <raquelalexandramendes@gmail.com>
2024-10-07 10:36:35 +0100

    DataBindingI

---
+++ Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiDemo.Models
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
